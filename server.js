d713473a0841c4cb81bbf8b064d0bba253f011f7
Kiel Sprague <aereisdin@gmail.com>
2021-03-11 14:03:32 -0800

    moved the .json package

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'))
+app.use(express.static('./Develop/public'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('../routes/apiRoutes')(app);
-require('../routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => {
+app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}`);
   });
